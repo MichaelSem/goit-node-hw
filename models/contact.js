24c5b6ddcce25d1344f5a084648cf8f6d9677997
Semeniuk Mykhailo <micanton0709@gmail.com>
2023-11-07 21:00:12 -0500

    .

---
+++ models/contact.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const mongoosePaginate = require('mongoose-paginate');
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Set name for contact'],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'user',
+    },
+  },
+  { versionKey: false }
+);
+
+contactSchema.plugin(mongoosePaginate);
+
+const Contact = mongoose.model('contact', contactSchema);
+
+module.exports = Contact;
---
+++ service/index.js
@@ -0,0 +1,4 @@
+const contactService = require('./contacts/contacts');
+const userService = require('./users/users');
+
+module.exports = { contactService, userService };
